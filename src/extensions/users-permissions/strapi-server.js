ad5dd6467af5137e4974da6536b6ef94a282fbfb
Ali Shabani <alishabani9270@gmail.com>
2022-03-15 14:29:04 +0330

    feat: added extended service for users-permissions plugin

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,6 +1,7 @@
 const omit = require("lodash/omit");
 const routes = require("./routes/routes");
 const authController = require("./controllers/auth-controller");
+const extendedService = require("./services/extended");
 
 module.exports = (plugin) => {
   plugin.controllers.auth = {
@@ -11,5 +12,6 @@ module.exports = (plugin) => {
   plugin.config = omit(plugin.config, "layout");
 
   plugin.routes["content-api"].routes.push(...routes.routes);
+  plugin.services.extended = extendedService;
   return plugin;
 };
