7839465837614fb04c4cd5d5946ff7bc638a387f
ali shabani <alishabani9270@gmail.com>
2022-04-12 12:46:09 +0430

    fix: fixed rahche notification child

--- src/api/notification/services/builder.js
+++ src/api/notification/services/builder.js
@@ -5,13 +5,14 @@ const nQuery = (strapi) => strapi.query("api::notification.notification");
 
 module.exports = ({ strapi }) => ({
   async rahche({ rahche }) {
+    console.log(rahche);
     await nService(strapi).create({
       data: {
         title: "rahche created",
         body: "rahce was created",
         type: "rahche",
         user: rahche.user.id,
-        childId: rahche.child.id,
+        child: rahche.child.id,
         payload: {
           id: rahche.id,
         },
