798d148ee91d8c327d696c48f505bd3578e0196e
ali shabani <alishabani9270@gmail.com>
2022-04-19 14:34:07 +0430

    fix: added new types for activity duration

--- src/api/activity/content-types/activity/lifecycles.js
+++ src/api/activity/content-types/activity/lifecycles.js
@@ -1,15 +1,33 @@
+var getYear = require("date-fns-jalali/getYear");
+
 const durations = {
   audio: 15,
   book: 20,
   game: 20,
+  activity: 30,
+  video: {
+    3: 60,
+    8: 120,
+  },
 };
 
 async function updateDuration(record, model) {
-  const { id, type } = record;
-  const duration = durations[type];
+  const { id, type, child } = record;
+  let duration = durations[type];
 
   if (!duration) {
     return;
+  }
+
+  if (typeof duration === "object") {
+    const age = getYear(new Date()) - child.birthYear;
+    const ageGroup = await strapi.service("api::age-group.extended").get(child);
+
+    duration = duration[ageGroup.slug];
+
+    if (!duration) {
+      return;
+    }
   }
 
   await strapi.service(model.uid).update(id, { data: { duration } });
