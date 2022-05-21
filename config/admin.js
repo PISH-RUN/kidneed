d6e1dab924e36891f240dd65090c2437ccb3adcb
ali shabani <alishabani9270@gmail.com>
2022-05-21 13:28:11 +0430

    fix: added salt apiToken

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
+const crypto = require("crypto");
+
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '3876d1c8e8c5cb3511f09ebb388c1bd3'),
+    secret: env("ADMIN_JWT_SECRET", "3876d1c8e8c5cb3511f09ebb388c1bd3"),
   },
+  apiToken: { salt: crypto.randomBytes(16).toString("base64") },
 });
